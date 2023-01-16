9fd67426d7be41fbfce84ecf8ef09baefbd5e139
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2023-01-16 18:59:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home/Home'
 import Share from './pages/share/Share';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Writeconfession from './pages/newconfession/Writeconfession';
 function App() {
   return (
-    <div className="App">
-  <Writeconfession/>
+    <Router basename={process.env.PUBLIC_URL} >
+
+
+
+  <div className="App">
+  <Routes>
+  <Route
+            path="/"
+            element={
+             <Home/>
+            }
+          ></Route>
+
+<Route
+            path="/:username"
+            element={
+             <Writeconfession/>
+            }
+          ></Route>
+  </Routes>
     </div>
+  
+       
+    </Router>
+   
   );
 }
 
--- src/pages/newconfession/Writeconfession.jsx
+++ src/pages/newconfession/Writeconfession.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './writeconfession.css'
-
+import { useParams } from 'react-router-dom';
 function Writeconfession() {
+const [username,setUsername]=useState(null)
+const params = useParams();
+    useEffect(()=>{
+
+         
+    
+
+    })
+const [theme,setTheme]=useState("eight");
+const [fcolor,setFcolor]=useState("black")
+    const selectTheme=(e)=>{
+
+
+if(e.target.name==="six")
+{
+    setFcolor("white")
+}
+else{
+    setFcolor("black")
+}
+setTheme(e.target.name)
+
+    }
+
+   if( username!=null && theme && fcolor)
     return (
+    
         <>
 
 
             <Navbar />
             <div className="writeconfessioncontainer">
                 <div className="writeconfessionsec1">
-                    <h1 className='writeconfessiontitle'>Confess your love</h1>
+                    <h1 className='writeconfessiontitle'>Confess your love to $$$$</h1>
                     <p className='writeconfessionsub'> Your identity is not revealed or stored with  { }</p>
-                    <textarea className="writeconfession" placeholder="confess your love to {   }" />
+                    <textarea  maxLength={800}  style={{backgroundImage: "url(" +  process.env.PUBLIC_URL+"/assets/images/"+theme+".png"  + ")", color:fcolor}} className="writeconfession" placeholder="confess your love to {   }" />
                     <button className="writeconfessionbtn">Share </button>
                 </div>
  
                <div className="writeconfessionsec2">
                     <h3 className='themetitle'>Choose theme</h3>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/one.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/two.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/three.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/four.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/five.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/six.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/seven.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/eight.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/nine.png'}/>
-                    <img className="writeconfessionthemeimage"  src={process.env.PUBLIC_URL+'/assets/images/ten.png'}/>
+                    <img className="writeconfessionthemeimage" name="one" onClick={(e)=>selectTheme(e)}  src={process.env.PUBLIC_URL+'/assets/images/one.png'}/>
+                    <img className="writeconfessionthemeimage" name="two" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/two.png'}/>
+                    <img className="writeconfessionthemeimage" name="three" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/three.png'}/>
+                    <img className="writeconfessionthemeimage" name="four" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/four.png'}/>
+                    <img className="writeconfessionthemeimage" name="five" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/five.png'}/>
+                    <img className="writeconfessionthemeimage" name="six" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/six.png'}/>
+                    <img className="writeconfessionthemeimage" name="seven" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/seven.png'}/>
+                    <img className="writeconfessionthemeimage" name="eight" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/eight.png'}/>
+                    <img className="writeconfessionthemeimage" name="nine" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/nine.png'}/>
+                    <img className="writeconfessionthemeimage" name="ten" onClick={(e)=>selectTheme(e)} src={process.env.PUBLIC_URL+'/assets/images/ten.png'}/>
 
 
 
@@ -36,6 +62,25 @@ function Writeconfession() {
             </div>
         </>
     )
+    else{
+        return(
+
+            <>
+
+
+            <Navbar />
+            <div className="writeconfessioncontainer">
+                <div className="writeconfessionsec1">
+                    <h1 className='writeconfessiontitle'>no confession room exists named $$$</h1>
+                    <p className='writeconfessionsub'> click here to create your own confession room</p>
+                  
+                </div>
+                </div>
+ 
+            </>
+
+        )
+    }
 }
 
 export default Writeconfession
